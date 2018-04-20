34bd0ec036b851f4820abe6ef985bfa0deafa70a
Rafael Mauricio Giraldo Gallego <rafagiga2022@gmail.com>
2018-04-20 02:04:45 -0500

    First project php
    This is php's homework due

---
+++ Analisis/php/exercise1php/index.php
@@ -0,0 +1,7 @@
+<?php 
+require 'message.php';
+use firtsProyect\message;
+$message = new message();
+$message->hello();
+$message1 = new message();
+$message1->messages();
---
+++ Analisis/php/exercise1php/message.php
@@ -0,0 +1,12 @@
+<?php 
+namespace firtsProyect ;
+class message{
+    public function hello(){
+        echo 'Hello, my name is Rafael Mauricio Giraldo Gallego</br>';
+    }
+    public function messages(){
+        echo 'This is my second message in PHP through object-oriented programming.';
+    }
+}
+
+
