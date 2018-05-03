7904464166dd7a07b761041ed8f41101dc263edb
Rafael Mauricio Giraldo Gallego <rafagiga2022@gmail.com>
2018-05-02 19:45:29 -0500

    PHP's vestiges
    Activity of the PHP's vestiges

---
+++ Analisis/php/phpWorkshop/point10.php
@@ -0,0 +1,5 @@
+<?php
+define("CONSTANTE", "definition of the constant.",true);
+echo CONSTANTE; // print "definition of the constant."
+echo constante; // print "Constant" A constant is an identifier (name) for a simple value. .
+
