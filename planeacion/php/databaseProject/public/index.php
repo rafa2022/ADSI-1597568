5fb22bb41a8fdd852a24d4403c32e564232224b4
Rafael Mauricio Giraldo Gallego <rafagiga2022@gmail.com>
2018-05-06 00:45:00 -0500

    This is connection to database
    Using sqli

---
+++ planeacion/php/databaseProject/public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <a href="img/favicon.ico"></a>
+    <title>DataBase</title>
+</head>
+<body>
+    <?php
+    // put your code here
+    ?>
+    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
